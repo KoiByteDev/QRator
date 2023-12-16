2b4b8a4cfb1b09d0b676fa32416a068deeccb8c6
Mendo94822 <koibytedev@gmail.com>
2023-12-15 23:29:19 -0500

    Transfered Files to Main

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import QRCodeGenerator from './assets/pages/QRCodeGenerator';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <QRCodeGenerator />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/BGColor.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const BGColor = () => {
+  return (
+    <div className='grid grid-cols-4 h-full w-full'>
+        <div className='w-full h-full  bg-white'>
+                      
+        </div>
+        <div className='w-full h-full  bg-black'>
+                      
+        </div>
+        <div className='w-full h-full  bg-white'>
+                      
+        </div>
+        <div className='w-full h-full  bg-black'>
+                      
+        </div>
+        <div className='w-full h-full  bg-black'>
+                      
+        </div>
+        <div className='w-full h-full  bg-white'>
+                      
+        </div>
+        <div className='w-full h-full  bg-black'>
+                      
+        </div>
+        <div className='w-full h-full  bg-white'>
+                      
+        </div>
+        <div className='w-full h-full  bg-white'>
+                      
+        </div>
+        <div className='w-full h-full  bg-black'>
+                      
+        </div>
+        <div className='w-full h-full  bg-white'>
+                      
+        </div>
+        <div className='w-full h-full  bg-black'>
+                      
+        </div>
+    </div>
+  )
+}
+
+export default BGColor
